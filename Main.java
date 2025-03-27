adde815db175c7bf0c18104ca6734a47aa9f33e1
whyatb <whyatb@gmail.com>
2025-03-27 16:06:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ TO DO:
 
 // enemy types are INT so to call them i.e. call enemy:1,2,3
 
-//nodeType controls whether room will be noncombat or combat, with number thresholds indicating enemy type I.E 0-1 is non-combat, anything higher determines type of enemy
+//roomType controls whether room will be noncombat or combat, with number thresholds indicating enemy type I.E 0-1 is non-combat, anything higher determines type of enemy
 
 
 // nextRoom is the default if there is only one way to go.
@@ -23,32 +23,32 @@ TO DO:
 
 //Navigation menu will read the values of nextRight and nextLeft and if either one is null, then menu will simply have a next-room option, otherwise present a choice
 
-// node type and description is constructed here
+// room type and description is constructed here
 
-class Node{
+class Room {
 
-    int nodeType;
+    int roomType;
     String Description;
 
-    Node nextLeft;
-    Node nextRight;
-    Node nextRoom;
+    Room nextLeft;
+    Room nextRight;
+    Room nextRoom;
 
-    public Node(int nodeType, String Description){
-        this.nodeType = nodeType;
+    public Room(int roomType, String Description){
+        this.roomType = roomType;
         this.Description = Description;
     }
 
-    public void connectLeft(Node nextLeftNode){
-        this.nextLeft = nextLeftNode;
+    public void connectLeft(Room nextLeftRoom){
+        this.nextLeft = nextLeftRoom;
     }
 
-    public void connectRight(Node nextRightNode){
-        this.nextRight = nextRightNode;
+    public void connectRight(Room nextRightRoom){
+        this.nextRight = nextRightRoom;
     }
 
-    public void connectRoom(Node nextNode){
-        this.nextRoom = nextNode;
+    public void connectRoom(Room nextRoom){
+        this.nextRoom = nextRoom;
     }
 }
 
@@ -65,54 +65,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Text-Based RPG\n------------------------------\n\nBegin game: yes/no");
+        // room type and description is stored here
 
-        // node type and description is stored here
+        Room r1 = new Room(0, "Outside");
+        Room r2 = new Room(0, "Entrance");
+        Room r3 = new Room(1, "Combat 1");
+        Room r4 = new Room(2, "Combat 2");
+        Room r5 = new Room(0, "Puzzle 1");
+        Room r6 = new Room(0, "Combat 3");
+        Room r7 = new Room(0, "room description");
+        Room r8 = new Room(0, "room description");
+        Room r9 = new Room(0, "room description");
+        Room r10 = new Room(0, "room description");
+        Room r11 = new Room(0, "room description");
+        Room r12 = new Room(0, "room description");
+        Room r13 = new Room(0, "room description");
+        Room r14 = new Room(0, "room description");
+        Room r15 = new Room(0, "room description");
 
-        Node r1 = new Node(0, "room description//Outside dungeon entrance");
-        Node r2 = new Node(0, "room description//Inside dungeon entrance");
-        Node r3 = new Node(1, "room description First Combat room");
-        Node r4 = new Node(2, "room description//First puzzle room");
-        Node r5 = new Node(0, "room description");
-        Node r6 = new Node(0, "room description");
-        Node r7 = new Node(0, "room description");
-        Node r8 = new Node(0, "room description");
-        Node r9 = new Node(0, "room description");
-        Node r10 = new Node(0, "room description");
-        Node r11 = new Node(0, "room description");
-        Node r12 = new Node(0, "room description");
-        Node r13 = new Node(0, "room description");
-        Node r14 = new Node(0, "room description");
-        Node r15 = new Node(0, "room description");
-
-        boolean playing  = true;
-        String enter = "";
-        Combat c1 = new Combat();
-
-        while(playing){
-            System.out.println(r1.Description);
-            r1.connectRoom(r2);
-            System.out.println(r2.Description);
-            r2.connectRoom(r3);
-            System.out.println(r3.Description);
-            if(r3.nodeType == 1){
-                c1.CombatLoop();
-                System.out.println("hi");
-            }
-            playing = false;
-        }
-
-
-
-        //r1.connectRoom(r2);
+        r1.connectRoom(r2);
         r2.connectRoom(r3);
+
         r3.connectLeft(r4);
         r3.connectRight(r5);
-        r4.connectRoom(r6);
 
+        r4.connectRoom(r6);
         r6.connectRoom(r8);
+
         r5.connectRoom(r7);
         r7.connectLeft(r8);
+
         r8.connectRight(r10);
         r10.connectLeft(r13);
         r10.connectRight(r12);
@@ -128,6 +110,30 @@ public class Main {
 
 
 
+
+        System.out.println("Welcome to the Text-Based RPG\n------------------------------\n\nBegin game: yes/no");
+
+        boolean playing  = true;
+        String enter = "";
+        Combat c1 = new Combat();
+
+        while(playing){
+            System.out.println(r1.Description);
+            r1.connectRoom(r2);
+            System.out.println(r2.Description);
+            r2.connectRoom(r3);
+            System.out.println(r3.Description);
+            if(r3.roomType == 1){
+                c1.CombatLoop();
+                System.out.println("hi");
+            }
+
+        }
+
+
+
+
+
         // opening desc.
         //
 
@@ -138,7 +144,7 @@ public class Main {
         //map looks like this [entrance => hallway => branchRoom => roomL OR roomR => entrance]
 
 
-        // the map will be a series of nodes that we draw out before hand is always the same
+        // the map will be a series of rooms that we draw out before hand is always the same
         // setting is low fantasy: think knights, dragons, magic, etc...
 
         // interface will be menu based, with options to move, attack, etc...
