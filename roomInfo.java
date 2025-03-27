fa45bc08daf65a212ee7e63d35883fd9b888dd8f
whyatb <whyatb@gmail.com>
2025-03-27 16:06:30 -0400

    Update roomInfo.java

--- roomInfo.java
+++ roomInfo.java
@@ -1,7 +1,9 @@
 public class roomInfo {
 
 
-
+// this call provide the loop that will run when each room is entered
+    // we need to figure out how to store a unique loop for each room that can be run each time
+    // this loop will require the use of the combat loop therefore we cannot finish the combat until this is implemented
 
 
 }
